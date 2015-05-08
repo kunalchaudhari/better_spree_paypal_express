e8fe867dcbe2ad0d8c128a9086c3ae91fed48c05
Kunal Chaudhari <kunalchaudhari@gmail.com>
2015-05-08 18:57:46 +0530

    authorize for paypal

--- app/models/spree/gateway/pay_pal_express.rb
+++ app/models/spree/gateway/pay_pal_express.rb
@@ -27,11 +27,41 @@ module Spree
     end
 
     def auto_capture?
-      true
+      Spree::Config[:auto_capture]
     end
 
     def method_type
       'paypal'
+    end
+
+    def authorize(amount, express_checkout, gateway_options={})
+      pp_request = provider.build_create_billing_agreement({
+        :Token => express_checkout.token
+      })
+      pp_response = provider.create_billing_agreement(pp_request)
+
+      if pp_response.success?
+        # We need to store the reference id for the future.
+        # This is mainly so we can use it later on to capture the payment.
+        reference_id = pp_response.billing_agreement_id
+        # This is rather hackish, required for payment/processing handle_response code.
+        Class.new do
+          def initialize(reference_id)
+            @reference_id = reference_id
+          end
+          def success?; true; end
+          def avs_result; {}; end
+          def cvv_result; nil; end
+          def authorization; @reference_id; end
+        end.new(reference_id)
+      else
+        class << pp_response
+          def to_s
+            errors.map(&:long_message).join(" ")
+          end
+        end
+        pp_response
+      end
     end
 
     def purchase(amount, express_checkout, gateway_options={})
@@ -55,6 +85,37 @@ module Spree
         # This is mainly so we can use it later on to refund the payment if the user wishes.
         transaction_id = pp_response.do_express_checkout_payment_response_details.payment_info.first.transaction_id
         express_checkout.update_column(:transaction_id, transaction_id)
+        # This is rather hackish, required for payment/processing handle_response code.
+        Class.new do
+          def success?; true; end
+          def authorization; nil; end
+        end.new
+      else
+        class << pp_response
+          def to_s
+            errors.map(&:long_message).join(" ")
+          end
+        end
+        pp_response
+      end
+    end
+
+    def capture(amount, auth_code, gateway_options={})
+      pp_details_request = provider.build_do_reference_transaction({
+        :DoReferenceTransactionRequestDetails => {
+          :ReferenceID => auth_code,
+          :PaymentAction => "Sale",
+          :PaymentDetails => {
+            :OrderTotal => {
+              :currencyID => gateway_options[:currency],
+              :value => amount
+            }
+          }
+        }
+      })
+
+      pp_response = provider.do_reference_transaction(pp_details_request)
+      if pp_response.success?
         # This is rather hackish, required for payment/processing handle_response code.
         Class.new do
           def success?; true; end
